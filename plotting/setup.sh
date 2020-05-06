a484d98e80327be87b1bf6d610a79396910d75c1
Josh Tingey <joshtingey93@gmail.com>
2020-05-06 16:55:02 +0100

    added python environment

---
+++ plotting/setup.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+CURRENTDIR=$(pwd)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+cd $DIR
+
+if [[ -d "./conda/" ]]; then
+    source ./conda/bin/activate
+    conda activate thesis
+else
+    # Download the latest version of miniconda3
+    wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh --no-check-certificate
+
+    # Install miniconda3 in the current directory
+    bash miniconda.sh -b -p $DIR/conda
+    rm miniconda.sh
+
+    # Activate miniconda and create the thesis environment
+    source conda/bin/activate
+    conda update -n base -c defaults conda -y
+    conda config --add envs_dirs $DIR/conda/envs
+    conda config --add envs_dirs $DIR/conda/envs
+    conda env create -f $DIR/environment.yaml
+
+    # Clean the miniconda install
+    conda clean --all -y
+
+    # Make sure the base environement is not enabled by default
+    conda config --set auto_activate_base false
+
+    conda activate thesis
+fi
+
+# Go back to the user directory
+cd $CURRENTDIR
+return
