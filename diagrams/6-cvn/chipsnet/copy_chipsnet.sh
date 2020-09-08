01ef1484bda3ab6dfec7f3e06550a28d8aa16669
Josh Tingey <joshtingey93@gmail.com>
2020-09-08 17:13:27 +0100

    updated cvn theory section and chips baseline implementation plots

--- diagrams/6-cvn/chipsnet/copy_chipsnet.sh
+++ diagrams/6-cvn/chipsnet/copy_chipsnet.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-rsync gpu2:/mnt/storage/jtingey/chips-net/data/output/*.pdf ./diagrams/7-cvn/chipsnet/
+rsync gpu2:/mnt/storage/jtingey/chips-net/data/output/*.pdf ./diagrams/6-cvn/chipsnet/
